075d5eb0c437938902c2082c8812d99e177a38ec
Ur Sok <marin.dabija@mail.ru>
2021-10-04 02:12:15 +0300

    Added RSA Encryption

---
+++ SI.RSAEncryption(LAB1)/Rsa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SI.RSAEncryption
+{
+    public class Rsa
+    {
+        private BigInteger e;
+        private BigInteger d;
+        private BigInteger n;
+
+        internal Rsa(BigInteger e, BigInteger d, BigInteger n)
+        {
+            this.e = e;
+            this.d = d;
+            this.n = n;
+        }
+
+        public BigInteger[] Encrypt(string message)
+        {
+            return message.ToCharArray().ToList()
+                .ConvertAll(x => BigInteger.ModPow(x, e, n)).ToArray();
+        }
+
+        public string Decrypt(BigInteger[] encryptedText)
+        {
+            var charArray = encryptedText.ToList()
+                .ConvertAll(x => (char)BigInteger.ModPow(x, d, n)).ToArray();
+            return new string(charArray);
+        }
+    }
+}
